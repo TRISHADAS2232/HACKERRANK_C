a01073998ac90ab78f27ea6dfc7e8280108f00a2
TRISHADAS2232 <93197391+TRISHADAS2232@users.noreply.github.com>
2021-10-27 20:09:20 +0530

    Create 5.pointers.c

---
+++ 5.pointers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function    
+}
+
+int main() {
+    int a;
+    int b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d\n", a+b);
+    printf("%d\n", abs(a-b));
+    return 0;
+}
